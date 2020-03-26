62898e54e245191cf6d5585eea82dcc700aea401
Bill D <355438+vegetabill@users.noreply.github.com>
2020-03-25 18:09:08 -0700

    improve error message generator

--- react-app/src/helpers/booktonica-api-fetcher.js
+++ react-app/src/helpers/booktonica-api-fetcher.js
@@ -1,5 +1,12 @@
+function generateErrorMessage(path, resp) {
+  return `😩 fetch('${path}') failed: Express server responded with HTTP: [${resp.status} ${resp.statusText}].
+  
+  (Note: this error is custom to Booktonica and you cannot Google it). Check your Network console for more information about the request and the Express logs for more information about the response.`;
+}
+
 export function addLike(bookId, username) {
-  return fetch(`/books/${bookId}/likes`, {
+  const path = `/books/${bookId}/likes`;
+  return fetch(path, {
     method: "POST",
     headers: {
       // This header is needed or React app won't proxy it along to Express
@@ -11,15 +18,14 @@ export function addLike(bookId, username) {
     if (resp.ok) {
       return true;
     } else {
-      throw new Error(
-        `😩 fetch('/books') failed: Express server responded with HTTP ${resp.status} ${resp.statusText}. (Note: this error is custom to Booktonica and you cannot Google it). Check your Network console for more information about the request and the Express logs for more information about the response.`
-      );
+      throw new Error(generateErrorMessage(path, resp));
     }
   });
 }
 
 export function getAllLikes() {
-  return fetch("/books/likes", {
+  const path = "/books/likes";
+  return fetch(path, {
     headers: {
       // This header is needed or React app won't proxy it along to Express
       Accept: "application/json"
@@ -28,15 +34,14 @@ export function getAllLikes() {
     if (resp.ok) {
       return resp.json();
     } else {
-      throw new Error(
-        `😩 fetch('/books') failed: Express server responded with HTTP ${resp.status} ${resp.statusText}. (Note: this error is custom to Booktonica and you cannot Google it). Check your Network console for more information about the request and the Express logs for more information about the response.`
-      );
+      throw new Error(generateErrorMessage(path, resp));
     }
   });
 }
 
 export function getAllBooks() {
-  return fetch("/books", {
+  const path = "/books";
+  return fetch(path, {
     headers: {
       // This header is needed or React app won't proxy it along to Express
       Accept: "application/json"
@@ -45,15 +50,14 @@ export function getAllBooks() {
     if (resp.ok) {
       return resp.json();
     } else {
-      throw new Error(
-        `😩 fetch('/books') failed: Express server responded with HTTP ${resp.status} ${resp.statusText}. (Note: this error is custom to Booktonica and you cannot Google it). Check your Network console for more information about the request and the Express logs for more information about the response.`
-      );
+      throw new Error(generateErrorMessage(path, resp));
     }
   });
 }
 
 export function getUser(username) {
-  return fetch(`/users/${username}`, {
+  const path = `/users/${username}`;
+  return fetch(path, {
     headers: {
       // This header is needed or React app won't proxy it along to Express
       Accept: "application/json"
@@ -62,9 +66,7 @@ export function getUser(username) {
     if (resp.ok) {
       return resp.json();
     } else {
-      throw new Error(
-        `😩 fetch('/books') failed: Express server responded with HTTP ${resp.status} ${resp.statusText}. (Note: this error is custom to Booktonica and you cannot Google it). Check your Network console for more information about the request and the Express logs for more information about the response.`
-      );
+      throw new Error(generateErrorMessage(path, resp));
     }
   });
 }
